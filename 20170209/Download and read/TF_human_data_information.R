d8b029256f34f81ac130184ae53b79121241bc0e
Chengshu21 <1981893234@qq.com>
2019-07-02 13:07:08 +0800

    Delete TF_human_data_information.R

--- 20170209/Download and read/TF_human_data_information.R
+++
@@ -1,9 +0,0 @@
-cstr_rt <- read.table("D:/Cistrome/TF_human_data_information.txt", head = TRUE, sep = "\t")
-str(cstr_rt)    
-head(cstr_rt)
-
-write.csv(cstr_rt, "D:/Cistrome/TF_human_data_information.csv")   #write a new file to make a copy.
-
-cstr_rt1 <- read.csv("D:/Cistrome/TF_human_data_information.csv", head = TRUE, sep = ",")
-str(cstr_rt1)
-head(cstr_rt1)
